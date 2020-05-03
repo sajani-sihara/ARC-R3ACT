45bd2fe1bbd98c76025e534ff63756c1b0598927
christina-thambirajah <58282529+christina-thambirajah@users.noreply.github.com>
2020-05-03 05:54:25 +0530

    Syntax error Fixed

---
+++ Server_side/build/precache-manifest.8a3b37aa1294d41a6a381febdcc08d0e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c573d8267666d8f48c1e339252eb050",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c4d2547a53aca87e8bf",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "528d22f8aeea7fec84aa",
+    "url": "/static/css/main.37b8443b.chunk.css"
+  },
+  {
+    "revision": "2c4d2547a53aca87e8bf",
+    "url": "/static/js/2.0996109f.chunk.js"
+  },
+  {
+    "revision": "005c97f82cee60e20eac659314ea29ec",
+    "url": "/static/js/2.0996109f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "528d22f8aeea7fec84aa",
+    "url": "/static/js/main.f86f9b46.chunk.js"
+  },
+  {
+    "revision": "a704f203812946f084b4",
+    "url": "/static/js/runtime-main.1d34c11e.js"
+  }
+]);
